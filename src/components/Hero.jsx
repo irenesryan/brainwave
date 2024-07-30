eac9c1ab2438909c2061114405c4f5cbf8506767
Irene Ryan <irene.sabrina@arkonit.se>
2024-07-30 11:39:32 +0200

    implementation of benefits component.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,6 +7,7 @@ import { ScrollParallax } from "react-just-parallax";
 import { useRef } from "react";
 import Generating from "./Generating";
 import Notification from "./Notification";
+import CompanyLogos from "./CompanyLogos";
 
 const Hero = () => {
   const parallaxRef = useRef(null);
@@ -54,7 +55,7 @@ const Hero = () => {
                   height={490}
                   alt="AI"
                 />
-                
+
                 <Generating className="absolute left-4 right-4 bottom-5 md:left-1/2 md:right-auto md:bottom-8 md:w-[31rem] md:-translate-x-1/2" />
 
                 <ScrollParallax isAbsolutelyPositioned>
@@ -88,6 +89,8 @@ const Hero = () => {
           </div>
           <BackgroundCircles />
         </div>
+
+        <CompanyLogos className="hidden relative z-10 mt-20 lg:block" />
       </div>
       <BottomLine />
     </Section>
