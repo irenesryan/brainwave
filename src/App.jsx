53ade403038b90171c573b2ae5aa1b023ae30622
Irene Ryan <irene.sabrina@arkonit.se>
2024-08-19 17:47:47 +0200

    new components added to the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Benefits from "./components/Benefits";
+import Collaboration from "./components/Collaboration";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Pricing from "./components/Pricing";
+import Services from "./components/Services";
 
 const App = () => {
   return (
@@ -10,6 +13,9 @@ const App = () => {
         <Header />
         <Hero />
         <Benefits />
+        <Collaboration />
+        <Services />
+        <Pricing />
       </div>
       <ButtonGradient />
     </>
